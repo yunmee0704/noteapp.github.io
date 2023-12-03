0d3e1d583ac99a9a82cda2711228861a0eb1c176
yunmee0704 <900704630c@gmail.com>
2023-12-03 14:33:50 +0900

    간단한 미디어쿼리작업

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var add_btn = document.querySelector('.add');//추가버튼
-var list_area = document.querySelector('.list-area');//투두리스트 영역
+var add_btn = document.querySelector('.add'); //추가버튼
+var list_area = document.querySelector('.list-area'); //투두리스트 영역
 
 
-function add_list(a){ //투두리스트에 요소와 내용 추가하는 내용 함수
+function add_list(a) { //투두리스트에 요소와 내용 추가하는 내용 함수
 	var li = `	<li>
 						<input type="radio" name="" id="" class="check">
 							 <p class="text-box">${a}</p>
@@ -11,21 +11,21 @@ function add_list(a){ //투두리스트에 요소와 내용 추가하는 내용 
 								<button class="delete">삭제</button>
 						</div>
 					</li>`
-		list_area.insertAdjacentHTML('beforeend', li);
-		 document.querySelector('.add-text').value = '';
+	list_area.insertAdjacentHTML('beforeend', li);
+	document.querySelector('.add-text').value = '';
 }
 
-function set_list(){
-	var todo_arr = localStorage.getItem('todo');//로컬스토리지에 있는 데이터 가져와서 변수에 넣기
+function set_list() {
+	var todo_arr = localStorage.getItem('todo'); //로컬스토리지에 있는 데이터 가져와서 변수에 넣기
 	todo_arr = JSON.parse(todo_arr);
-	if(todo_arr !== null){	
-		for( i = 0; i < todo_arr.length; i++){	
+	if (todo_arr !== null) {
+		for (i = 0; i < todo_arr.length; i++) {
 			add_list(todo_arr[i]); // 투두리스트를 로컬스토리지 데이터 내용으로 그려줌.
 		}
-	
-}else{
-	todo_arr = []; //로컬스토리지 내용이 없으면 빈배열로 정의.
-}
+
+	} else {
+		todo_arr = []; //로컬스토리지 내용이 없으면 빈배열로 정의.
+	}
 }
 
 set_list();
@@ -34,50 +34,54 @@ set_list();
 
 
 //로컬스토리지에 추가
-add_btn.addEventListener('click',function(){
-	todo_arr=localStorage.getItem('todo');
+add_btn.addEventListener('click', function () {
+	todo_arr = localStorage.getItem('todo');
 	todo_arr = JSON.parse(todo_arr);
-	var add_text = document.querySelector('.add-text').value;//input 창 내용
-	if(add_text === ''){
+	var add_text = document.querySelector('.add-text').value; //input 창 내용
+	if (add_text === '') {
 		return false; // 인풋 창에 아무것도 없으면 작동안함
-	}else {		
+	} else {
 		todo_arr.push(add_text); //추가하는 내용 투두리스트 배열에 추가
-	
 
-	// 로컬스토리지 세팅 추가기능 함수로 저장
-	todo_arr2 = JSON.stringify(todo_arr); //추가된 투두리스트 json형태로
-	localStorage.setItem('todo',todo_arr2); // 로컬스토리지에 todo 키 값의 배열 세팅
-		list_area.innerHTML ='';//전체리스트 지운 후 
-		set_list();//추가된 내용이 있는 리스트로 다시 그려줌.
+
+		// 로컬스토리지 세팅 추가기능 함수로 저장
+		todo_arr2 = JSON.stringify(todo_arr); //추가된 투두리스트 json형태로
+		localStorage.setItem('todo', todo_arr2); // 로컬스토리지에 todo 키 값의 배열 세팅
+		list_area.innerHTML = ''; //전체리스트 지운 후 
+		set_list(); //추가된 내용이 있는 리스트로 다시 그려줌.
 	}
 });
 
 var edit_btn = document.querySelectorAll('.edit');
 var delete_btn = document.querySelectorAll('.delete');
-var check_btn =document.querySelectorAll('.check');
+var check_btn = document.querySelectorAll('.check');
 
 //완료체크
-check_btn.forEach(function(el){		
-	el.addEventListener('click',function(){
-		el.parentElement.children[1].style.textDecoration='line-through';
-		console.log(el.parentElement.children[1])
+check_btn.forEach(function (el) {
+	el.addEventListener('click', function () {
+		el.parentElement.children[1].style.textDecoration = 'line-through';
 	})
 });
 
 
-	//삭제
-	delete_btn.forEach(function (el,index) {
-		el.addEventListener('click', function () {
-			todo_arr=localStorage.getItem('todo');
-			todo_arr = JSON.parse(todo_arr);
-      todo_arr.splice(index,1);
-			todo_arr2 =JSON.stringify(todo_arr);
-			localStorage.setItem('todo',todo_arr2);
-			list_area.innerHTML ='';//전체리스트 지운 후 
-			set_list();
-		})		
-	});
+//삭제
 
+
+
+delete_btn.forEach(function (el, index) {
+	el.addEventListener('click', function () {		
+		todo_arr = localStorage.getItem('todo');
+		todo_arr = JSON.parse(todo_arr);
+		todo_arr.splice(index, 1);
+		todo_arr2 = JSON.stringify(todo_arr);
+		localStorage.setItem('todo', todo_arr2);
+		list_area.innerHTML = ''; //전체리스트 지운 후 
+		set_list();
+	});
+	console.log(delete_btn)
+	return delete_btn;
+
+});
 
 
 // add_btn.addEventListener('click', function () {
